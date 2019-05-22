64b128e7badff4b0351eb6f94393c9d1f4986351
Alex Klimenkov <Alex@Alexanders-MacBook-Pro-3.local>
2019-05-23 01:34:27 +0300

    use class instead of module

--- lib/graphql/cache_context.rb
+++ lib/graphql/cache_context.rb
@@ -1,27 +1,25 @@
 require 'graphql/cache_context/version'
 require 'graphql/cache_context/store'
 
-module Graphql
-  class CacheContext
-    def use(schema_definition)
-      schema_definition.instrument(:field, self)
-    end
+class Graphql::CacheContext
+  def use(schema_definition)
+    schema_definition.instrument(:field, self)
+  end
 
-    def instrument(type, field)
-      old_resolve_proc = field.resolve_proc
+  def instrument(type, field)
+    old_resolve_proc = field.resolve_proc
 
-      new_resolve_proc = ->(object, arguments, context) do
+    new_resolve_proc = ->(object, arguments, context) do
 
-        # it means we want to get cache object
-        if old_resolve_proc.is_a?(Proc) && old_resolve_proc.arity == 4
-          cache = Graphql::CacheContext::Store.new(context)
-          old_resolve_proc.call(object, arguments, context, cache)
-        else
-          old_resolve_proc.call(object, arguments, context)
-        end
+      # it means we want to get cache object
+      if old_resolve_proc.is_a?(Proc) && old_resolve_proc.arity == 4
+        cache = Graphql::CacheContext::Store.new(context)
+        old_resolve_proc.call(object, arguments, context, cache)
+      else
+        old_resolve_proc.call(object, arguments, context)
       end
-
-      field.redefine { resolve(new_resolve_proc) }
     end
+
+    field.redefine { resolve(new_resolve_proc) }
   end
 end
